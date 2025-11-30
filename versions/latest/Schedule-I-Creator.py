181243866c7b73b0d85ddb89512f979972d4e352
Chaosflo677 <florian.t.winter@gmail.com>
2025-11-30 20:26:29 +0100

    first version of updater implemented

--- versions/latest/Schedule-I-Creator.py
+++ versions/latest/Schedule-I-Creator.py
@@ -1,4 +1,4 @@
-import os, datetime, base64, io, sys
+import os, datetime, base64, io, sys, requests
 import tkinter as tk
 from PIL import ImageTk, Image
 path=os.getcwd()
@@ -22,18 +22,15 @@ except:
     log("imagelib missing")
     log("terminating...")
     sys.exit("no imagelib")
-global version
 
 try:
     import config
-    version = config.version
     log("config loaded")
 except:
     log("no config found loading default")
-    version = "0.0.0"
 
 def saveconfig():
-    configfile="version="+str(version)+"\n"
+    configfile="\n"
     log("new config\n\n"+configfile)
     file= open(path+"\\config.py", "w")
     log("writing config file")
@@ -41,11 +38,25 @@ def saveconfig():
     log("sucsessfuly wrote config file")
 log("registerd function saveconfig")
 def update():
+    
+    open("updater.py", 'wb').write(requests.get("https://raw.githubusercontent.com/Chaosflo677/Schedule-I-creator/main/versions/latest/updater.py", allow_redirects=True).content)
+    log("updating updater")
     os.system("python "+path+"\\updater.py")
     log("updating")
+    sys.exit()
 log("registerd function update")
 saveconfig()
 root = tk.Tk()
 img = ImageTk.PhotoImage(Image.open(io.BytesIO(base64.b64decode(imagelib.appimg))))
 root.wm_iconphoto(False, img)
+menu_bar = tk.Menu(root)
+workspaces = tk.Menu(menu_bar, tearoff=0)
+SICreatorconfig = tk.Menu(menu_bar, tearoff=0)
+workspaces.add_command(label="new")
+workspaces.add_command(label="delete")
+SICreatorconfig.add_command(label="wiki")
+SICreatorconfig.add_command(label="update")
+menu_bar.add_cascade(label="workspaces", menu=workspaces)
+menu_bar.add_cascade(label="SICreator", menu=SICreatorconfig)
+root.config(menu=menu_bar)
 root.mainloop()
--- versions/latest/updater.py
+++ versions/latest/updater.py
@@ -1,12 +1,36 @@
-import subprocess,sys,requests,os
+import requests,os,base64,io,imagelib,sys,tkinter as tk
+from tkinter import font
+from PIL import ImageTk, Image
 path=os.getcwd()
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-install("pillow")
-version:str
-sourcerepo="https://raw.githubusercontent.com/Chaosflo677/Schedule-I-creator/main/versions/"+version+"/"
+global version
+version="latest"
+sourcerepo="https://raw.githubusercontent.com/Chaosflo677/Schedule-I-creator/main/versions/"
 def getfile(file):
-    r = requests.get(sourcerepo+file+".py", allow_redirects=True)
+    r = requests.get(sourcerepo+version+"/"+file+".py", allow_redirects=True)
     open(file+".py", 'wb').write(r.content)
-getfile("imagelib")
-getfile("Schedule-I-Creator")
+def update():
+    getfile("imagelib")
+    getfile("Schedule-I-Creator")
+    sys.exit()
+def on_selection(event):
+    selected_indices = versions.curselection()
+    for index in selected_indices:
+        version=versions.get(index)
+        button['text'] = 'install '+version
+updater=tk.Tk()
+img = ImageTk.PhotoImage(Image.open(io.BytesIO(base64.b64decode(imagelib.appimg))))
+updater.wm_iconphoto(False, img)
+updater.title("updater")
+updater.geometry("500x200")
+updater.resizable(False,False)
+tk.Label(updater,text="Select a version to install",font=font.Font(size=24)).pack()
+versions=tk.Listbox(updater,height=5)
+versions.pack()
+versions.insert(tk.END, "latest")
+versions.insert(tk.END, "1.0.0")
+versions.bind("<<ListboxSelect>>", on_selection)
+button = tk.Button(updater,
+	text = 'latest',
+	command = update)  
+button.pack()
+updater.mainloop()
